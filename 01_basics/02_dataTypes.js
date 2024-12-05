1a64699797e5269e404b3cc28d3a3848143a39f6
Bhushan Pandagre <143281492+BhushanPandagre@users.noreply.github.com>
2024-12-05 11:38:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all javascript code  as newer version or new js
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3 + 3); // code redability should be high
+
+console.log("bhushan");
+
+let age = 23;
+// data types
+
+// number => 2 to power 53
+// bigInt =>
+// string =>""
+// boolean=> true or false
+// null   => standalone value hai
+// undefined => value abhi assign nhi hui hai
+// symbol  => uniqueness
+
+// object
+
+console.log(typeof "bhushan"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
