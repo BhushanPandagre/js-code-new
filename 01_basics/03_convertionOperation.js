c5cd86217752b3976e8c6773571d38cc0912d1ba
Bhushan Pandagre <143281492+BhushanPandagre@users.noreply.github.com>
2024-12-05 12:33:58 +0000

    convertion of datatypes

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,29 @@
+let score = 33;
+
+// console.log(typeof score);//number
+// console.log(typeof (score));//number
+let isNumber = String(score);
+console.log(isNumber); //33
+console.log(typeof isNumber); //string
+
+// let mark = "33abc";
+//let mark = null //output= 0
+//let mark = undefined //NaN
+//let mark = true //1
+//let mark = false //0
+let mark = "bhushan"; // NaN
+
+// console.log(typeof mark);//string
+// console.log(typeof (mark));//string
+
+let valueInNumber = Number(mark);
+// console.log(typeof (valueInNumber));//number
+// console.log(valueInNumber);//NaN..not a number
+
+// let isLoggedIn = 1; // true
+//let isLoggedIn = 0; // false
+let isLoggedIn = ""; //false
+//  let isLoggedIn = "bhushan" //true
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
