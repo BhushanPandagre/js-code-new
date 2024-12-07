c322cd6c4247efbe0bc2117bf2b4a68bf09e008b
Bhushan Pandagre <143281492+BhushanPandagre@users.noreply.github.com>
2024-12-07 11:46:34 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "Bhushan pandagre ";
+const repoCount=70
+
+//console.log(name + repoCount + " value ");//old one 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//new one 
