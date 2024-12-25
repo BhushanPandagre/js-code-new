6d1494bfb579bae8d7a73fb9f05417543d636bcf
Bhushan Pandagre <143281492+BhushanPandagre@users.noreply.github.com>
2024-12-25 08:11:41 +0000

    function are running

--- 03_basics/01_functionsinjs.js
+++ 03_basics/01_functionsinjs.js
@@ -42,3 +42,39 @@ console.log(logginUserMessege("bhushan"));
 // console.log(logginUserMessege("Bhushan"))
 //Bhushan just Logged In
 console.log(addTwoNumbers("")); //undefined
+
+//new use csase rest operator
+//[ 100, 200, 300, 300, 400 ] return a array
+function calculatePrice(...item1) {
+  return item1;
+}
+console.log(calculatePrice(100, 200, 300, 300, 400));
+
+//========================
+const user = {
+  userName: "Bhushan",
+  age: 24,
+  price: 999,
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `user Name is ${anyObject.userName} and user age is ${anyObject.age} and item price is ${anyObject.price}`
+  );
+}
+// handleObject(user);
+handleObject({
+  userName: "Bharti",
+  age: 23,
+  price: 500,
+});
+
+//============================
+
+const myNewArray = [200, 300, 400];
+
+function returnSecondvalue(getArray) {
+  return getArray[2];
+}
+// console.log(returnSecondvalue(myNewArray));
+console.log(returnSecondvalue([200, 300, 600]));
