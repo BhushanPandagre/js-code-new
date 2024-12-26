16c0ec51e972c0a8128a59a031cc24a8f14b129c
Bhushan Pandagre <143281492+BhushanPandagre@users.noreply.github.com>
2024-12-26 20:59:43 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+//switch case
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("Januaray");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  default:
+    break;
+}
