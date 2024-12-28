0642c296da1413d0fa6d11cc87d1e7ddc0ca59c3
Bhushan Pandagre <143281492+BhushanPandagre@users.noreply.github.com>
2024-12-28 21:14:42 +0000

    while or do while loop

---
+++ 05_iterations/02_loop.js
@@ -0,0 +1,32 @@
+// while loop
+// let index = 2;
+
+// while(index<=10){
+//     console.log(`index is :${index}`)
+//     index = index+2;
+
+// }
+
+let j = 0;
+while (j <= 20) {
+  console.log(`Value of j is :${j}`);
+  j = j + 2;
+}
+
+let myArray = ["flash", "batman", "superman", "saktiman", "nagraj"];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+  //   console.log(`Value of array is :${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+// do while loop
+
+//ye one time too value print karta hi hai
+let a = 11;
+do {
+  console.log(`Value of A:${a}`);
+  a++;
+} while (a <= 10);
